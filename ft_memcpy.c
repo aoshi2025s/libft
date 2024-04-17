eeb1a682395dc8fe2a7a8f377fd467904dae3f95
aoshi2025s <aoshi2025s@gmail.com>
2024-04-17 23:46:05 +0900

    shuusei

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,18 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yoaoki <yoaoki@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/17 20:02:08 by yoaoki            #+#    #+#             */
-/*   Updated: 2024/04/17 22:41:07 by yoaoki           ###   ########.fr       */
+/*   Created: 2024/04/17 22:51:13 by yoaoki            #+#    #+#             */
+/*   Updated: 2024/04/17 23:45:53 by yoaoki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_bzero(void *s, size_t n)
+void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	ft_memset(s, 0, n);
+	unsigned char	*result_dst;
+	unsigned char	*result_src;
+
+	result_dst = (unsigned char *)dst;
+	result_src = (unsigned char *)src;
+	while (n > 0)
+	{
+		*result_dst++ = *result_src++;
+		n--;
+	}
+	return (dst);
 }
--- ft_memmove.c
+++ ft_memmove.c
@@ -6,8 +6,23 @@
 /*   By: yoaoki <yoaoki@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 20:02:27 by yoaoki            #+#    #+#             */
-/*   Updated: 2024/04/17 20:02:33 by yoaoki           ###   ########.fr       */
+/*   Updated: 2024/04/17 23:44:46 by yoaoki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	unsigned char *temp1;
+	unsigned char *temp2;
+
+	temp1 = dst;
+	temp2 = (unsigned char*)src;
+	if (dst < src)
+		return (ft_memcpy(dst, src, len));
+	if (dst > src)
+		while (len--)
+			temp1[len] = temp2[len];
+	return (dst);
+}
