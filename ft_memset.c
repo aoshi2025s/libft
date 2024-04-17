e298faba3e14faa188da337a774cb78b962135b3
aoshi2025s <aoshi2025s@gmail.com>
2024-04-17 22:26:41 +0900

    ft_memset ok

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: yoaoki <yoaoki@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 20:01:53 by yoaoki            #+#    #+#             */
-/*   Updated: 2024/04/17 22:21:56 by yoaoki           ###   ########.fr       */
+/*   Updated: 2024/04/17 22:26:13 by yoaoki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,15 @@
 
 void	*ft_memset(void *b, int c, size_t len)
 {
+	unsigned char	*result;
+
+	result = (unsigned char *)b;
+	while (len > 0)
+	{
+		*result = (unsigned char)c;
+		result++;
+		len--;
+	}
+	return (b);
+}
 
