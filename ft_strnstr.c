2934c694f5698245b27fa8831ae914606f0cd82e
aoshi2025s <aoshi2025s@gmail.com>
2024-04-18 21:13:37 +0900

    strnstr totyuu

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,8 +6,13 @@
 /*   By: yoaoki <yoaoki@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 20:05:15 by yoaoki            #+#    #+#             */
-/*   Updated: 2024/04/17 20:05:21 by yoaoki           ###   ########.fr       */
+/*   Updated: 2024/04/18 18:15:44 by yoaoki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+char	*strnstr(const char *haystack, const char *needle, size_t len)
+{
+
+}
