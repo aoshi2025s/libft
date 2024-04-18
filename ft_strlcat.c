a53e9cf8f8299df9a542262eed0fbfcec829f61a
aoshi2025s <aoshi2025s@gmail.com>
2024-04-18 16:04:25 +0900

    strlcpy strlcat ok

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,8 +6,31 @@
 /*   By: yoaoki <yoaoki@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 20:03:00 by yoaoki            #+#    #+#             */
-/*   Updated: 2024/04/17 20:03:12 by yoaoki           ###   ########.fr       */
+/*   Updated: 2024/04/18 16:02:55 by yoaoki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	srclen;
+	size_t	dstlen;
+	size_t	i;
+	
+	srclen = ft_strlen(src);
+	dstlen = ft_strlen(dst);
+	i = dstlen;
+
+	if (dstlen >= dstsize)
+		return (srclen + dstsize);
+
+	while (i < dstsize - 1 && *src)
+	{
+		dst[i] = *src;
+		i++;
+		src++;
+	}
+	dst[i] = '\0';
+	return (dstlen + srclen);
+}
--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,8 +6,33 @@
 /*   By: yoaoki <yoaoki@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 20:02:45 by yoaoki            #+#    #+#             */
-/*   Updated: 2024/04/17 20:02:55 by yoaoki           ###   ########.fr       */
+/*   Updated: 2024/04/18 16:03:40 by yoaoki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	len;
+	size_t	i;
+
+	len = ft_strlen(src);
+	if (dstsize == 0)
+		return (len);
+	i = 0;
+	while (i < len && i < dstsize - 1)
+	{
+		*dst++ = *src++;
+		i++;
+	}
+	*dst = '\0';
+	/*
+	while (i < dstsize)
+	{
+		*dst++ = '\0';
+		i++;
+	}
+	*/
+	return (len);
+}
