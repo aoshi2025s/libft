680ac520b25f1cc537da96956fe90fefd6ad7314
aoshi2025s <aoshi2025s@gmail.com>
2024-04-18 16:28:47 +0900

    strchr ok

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,8 +6,26 @@
 /*   By: yoaoki <yoaoki@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 20:04:04 by yoaoki            #+#    #+#             */
-/*   Updated: 2024/04/17 20:04:09 by yoaoki           ###   ########.fr       */
+/*   Updated: 2024/04/18 16:25:24 by yoaoki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+char	*ft_strchr(const char *str, int c)
+{
+	char	*curr;
+	char	altc;
+	altc = (char)c;
+	
+	curr = (char *)str;
+	while (*curr)
+	{
+		if (*curr == altc)
+			return (curr);
+		curr++;
+	}
+	if (altc == '\0')
+		return (curr);
+	return (0);
+}
