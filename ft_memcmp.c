db15b8408bbd4acb3cca466a7eb3538283a19edc
aoshi2025s <aoshi2025s@gmail.com>
2024-04-18 18:13:41 +0900

    memcmp ok

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,8 +6,26 @@
 /*   By: yoaoki <yoaoki@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 20:04:52 by yoaoki            #+#    #+#             */
-/*   Updated: 2024/04/17 20:05:08 by yoaoki           ###   ########.fr       */
+/*   Updated: 2024/04/18 18:12:41 by yoaoki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned char	*str1;
+	unsigned char	*str2;
+
+	str1 = (unsigned char*)s1;
+	str2 = (unsigned char*)s2;
+	while (n > 0)
+	{
+		if (*str1 != *str2)
+			return (*str1 - *str2);
+		str1++;
+		str2++;
+		n--;
+	}
+	return (0);
+}
