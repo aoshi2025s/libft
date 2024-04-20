3f4ff4e7df6230546b1543c6a120f0ec5b9bf565
aoshi2025s <aoshi2025s@gmail.com>
2024-04-21 02:13:45 +0900

    strnstr maybe ok

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,13 +6,38 @@
 /*   By: yoaoki <yoaoki@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 20:05:15 by yoaoki            #+#    #+#             */
-/*   Updated: 2024/04/18 22:34:53 by yoaoki           ###   ########.fr       */
+/*   Updated: 2024/04/21 02:12:14 by yoaoki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*strnstr(const char *haystack, const char *needle, size_t len)
+int	has_str(const char *s1, const char *s2)
 {
+	int	s2_len;
 
+	s2_len = ft_strlen(s2);
+	while (s2_len)
+	{
+		if (*s1 != *s2)
+			return (0);
+		s2_len--;
+		s1++;
+		s2++;
+	}
+	return (1);
+}
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	if (!(*needle))
+		return ((char *)haystack);
+	while (*haystack && len)
+	{
+		if (has_str(haystack, needle))
+			return ((char *)haystack);
+		haystack++;
+		len--;
+	}
+	return (0);
 }
