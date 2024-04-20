4b00b3c741950fffe00414fa4ca66b6e486a009b
aoshi2025s <aoshi2025s@gmail.com>
2024-04-21 03:50:26 +0900

    atoi all ok

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: yoaoki <yoaoki@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 20:05:27 by yoaoki            #+#    #+#             */
-/*   Updated: 2024/04/21 02:50:48 by yoaoki           ###   ########.fr       */
+/*   Updated: 2024/04/21 03:50:03 by yoaoki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,42 @@ int	ft_isspace(char c)
 		|| c == ' ');
 }
 
+unsigned long long	calcu_atoi(unsigned long long result, const char *str)
+{
+	while (ft_isdigit(*str))
+	{
+		result = result * 10 + (*str - '0');
+		if (result > LONG_MAX)
+			break ;
+		str++;
+	}
+	return (result);
+}
+
+int	ft_atoi(const char *str)
+{
+	unsigned long long	result;
+	int					minus;
+
+	result = 0;
+	minus = 1;
+	while (ft_isspace(*str))
+		str++;
+	if (*str == '-')
+	{
+		minus = -1;
+		str++;
+	}
+	else if (*str == '+')
+		str++;
+	result = calcu_atoi(result, str);
+	if (minus == -1 && result > LONG_MAX)
+		return (0);
+	else if (minus == 1 && result >= LONG_MAX)
+		return (-1);
+	return ((int)result * minus);
+}
+/*
 int	ft_atoi(const char *str)
 {
 	int	result;
@@ -41,3 +77,4 @@ int	ft_atoi(const char *str)
 	}
 	return (result * minus);
 }
+*/
