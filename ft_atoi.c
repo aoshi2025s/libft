92e5d32e068d8923b4263a9364445a723b48cf0e
aoshi2025s <aoshi2025s@gmail.com>
2024-04-21 02:57:52 +0900

    atoi madamada

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,8 +6,38 @@
 /*   By: yoaoki <yoaoki@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 20:05:27 by yoaoki            #+#    #+#             */
-/*   Updated: 2024/04/17 20:05:33 by yoaoki           ###   ########.fr       */
+/*   Updated: 2024/04/21 02:50:48 by yoaoki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+int	ft_isspace(char c)
+{
+	return (c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == '\r'
+		|| c == ' ');
+}
+
+int	ft_atoi(const char *str)
+{
+	int	result;
+	int	minus;
+
+	result = 0;
+	minus = 1;
+	while (ft_isspace(*str))
+		str++;
+	if (*str == '-')
+	{
+		minus = -1;
+		str++;
+	}
+	else if (*str == '+')
+		str++;
+	while (*str && ft_isdigit(*str))
+	{
+		result = result * 10 + (*str - '0');
+		str++;
+	}
+	return (result * minus);
+}
