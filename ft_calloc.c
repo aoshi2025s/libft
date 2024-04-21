b2f522040913604beb6b77a6279956e12f3d45be
aoshi2025s <aoshi2025s@gmail.com>
2024-04-22 00:58:59 +0900

    calloc maybe ok?

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,8 +6,19 @@
 /*   By: yoaoki <yoaoki@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 20:06:07 by yoaoki            #+#    #+#             */
-/*   Updated: 2024/04/17 20:06:15 by yoaoki           ###   ########.fr       */
+/*   Updated: 2024/04/22 00:52:21 by yoaoki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ptr;
+
+	ptr = malloc(count * size);
+	if (!ptr)
+		return (NULL);
+	ft_memset(ptr, 0, count);
+	return (ptr);
+}
