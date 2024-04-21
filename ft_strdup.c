3583643f1d6d5f368bfed8cbd2093dda4516ce26
aoshi2025s <aoshi2025s@gmail.com>
2024-04-22 01:29:34 +0900

    strdup ok

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: yoaoki <yoaoki@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 20:06:24 by yoaoki            #+#    #+#             */
-/*   Updated: 2024/04/22 01:14:41 by yoaoki           ###   ########.fr       */
+/*   Updated: 2024/04/22 01:28:35 by yoaoki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,11 @@
 
 char	*ft_strdup(const char *s1)
 {
-	size_t	len;
 	char	*result;
 
-	len = ft_strlen(s1);
-	result = (char *)malloc(len + 1);
+	result = (char *)malloc(ft_strlen(s1) + 1);
 	if (!result)
 		return (NULL);
-	ft_strlcpy(result, s1, len + 1);
+	ft_strlcpy(result, s1, ft_strlen(s1)+1);
 	return (result);
 }
