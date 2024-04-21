414823e65041d4bfcc67811fd672a6db2096efa2
aoshi2025s <aoshi2025s@gmail.com>
2024-04-22 01:15:18 +0900

    strdup maybe ok

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,8 +6,21 @@
 /*   By: yoaoki <yoaoki@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 20:06:24 by yoaoki            #+#    #+#             */
-/*   Updated: 2024/04/17 20:06:31 by yoaoki           ###   ########.fr       */
+/*   Updated: 2024/04/22 01:14:41 by yoaoki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+char	*ft_strdup(const char *s1)
+{
+	size_t	len;
+	char	*result;
+
+	len = ft_strlen(s1);
+	result = (char *)malloc(len + 1);
+	if (!result)
+		return (NULL);
+	ft_strlcpy(result, s1, len + 1);
+	return (result);
+}
