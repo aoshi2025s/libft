2557637f0ef857cf83acd7d7a649c1d545b6110c
aoshi2025s <aoshi2025s@gmail.com>
2024-04-22 17:12:28 +0900

    wakaran substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: yoaoki <yoaoki@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 15:25:57 by yoaoki            #+#    #+#             */
-/*   Updated: 2024/04/22 16:56:46 by yoaoki           ###   ########.fr       */
+/*   Updated: 2024/04/22 17:12:09 by yoaoki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char const	*start_s;
 
 	result = (char *)malloc(len + 1);
-	if (!result)
+	if (!result || (start >= ft_strlen(s)))
 		return (NULL);
 	start_s = &s[start];
 	ft_strlcpy(result, start_s, len + 1);
