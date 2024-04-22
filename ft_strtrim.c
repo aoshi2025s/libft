9afcb4f013be1b6e6fdfac105098514e99ae6afd
aoshi2025s <aoshi2025s@gmail.com>
2024-04-22 22:58:30 +0900

    strtrim 90% ok

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -5,60 +5,30 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: yoaoki <yoaoki@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/22 17:38:42 by yoaoki            #+#    #+#             */
-/*   Updated: 2024/04/22 19:02:32 by yoaoki           ###   ########.fr       */
+/*   Created: 2024/04/22 22:03:58 by yoaoki            #+#    #+#             */
+/*   Updated: 2024/04/22 22:57:25 by yoaoki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	is_exist_char(char c, char const *set)
-{
-	while (*set)
-	{
-		if (c == *set)
-			return (1);
-		set++;
-	}
-	return (0);
-}
-
-size_t	ft_strtrim_len(char const *s1, char const *set)
-{
-	size_t	len;
-	
-	len = 0;
-	while (*s1)
-	{
-		if (!is_exist_char(*s1, set))
-			len++;
-		s1++;
-	}
-	return (len);
-}
-
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*result;
-	char	*curr;
-	
-	if (!s1)
+	size_t	start;
+	size_t	end;
+
+	if (!s1 || !set)
 		return ((char *)malloc(1));
-	if (!set)
-		return (ft_strdup(s1));
-	result = (char *)malloc(ft_strtrim_len(s1, set) + 1);
+	start = 0;
+	end = ft_strlen(s1) - 1;
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	while (end > start && ft_strchr(set, s1[end]))
+		end--;
+	result = (char *)malloc(sizeof(char) * (end - start + 2));
 	if (!result)
-		return (NULL);
-	curr = result;
-	while (*s1)
-	{
-		if (!is_exist_char(*s1, set))
-		{
-			*curr = *s1;
-			curr++;
-		}
-		s1++;
-	}
-	*curr = '\0';
+		return (0);
+	ft_strlcpy(result, &s1[start], end - start + 2);
 	return (result);
 }
