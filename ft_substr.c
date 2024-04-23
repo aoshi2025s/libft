e1f486c6aa15067fd8b2bf8e8b410e25eedf073b
aoshi2025s <aoshi2025s@gmail.com>
2024-04-23 18:53:11 +0900

    substr protected

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: yoaoki <yoaoki@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 15:25:57 by yoaoki            #+#    #+#             */
-/*   Updated: 2024/04/22 17:48:02 by yoaoki           ###   ########.fr       */
+/*   Updated: 2024/04/23 18:51:46 by yoaoki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,11 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char		*result;
 	char const	*start_s;
 
+	if (!s)
+		return (0);
 	result = (char *)malloc(len + 1);
 	if (!result)
-		return (NULL);
+		return (0);
 	if (start >= ft_strlen(s) || len == 0)
 		return (result);
 	start_s = &s[start];
