0a40e1cff09256c57502d4be0c61ffb13bcf7ba5
aoshi2025s <aoshi2025s@gmail.com>
2024-04-24 01:11:15 +0900

    lstdelone update

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: yoaoki <yoaoki@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 00:58:05 by yoaoki            #+#    #+#             */
-/*   Updated: 2024/04/24 01:04:08 by yoaoki           ###   ########.fr       */
+/*   Updated: 2024/04/24 01:10:39 by yoaoki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,7 @@
 
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
-	if (!del)
-		return ;
-	if (!lst)
+	if (!del || !lst)
 		return ;
 	(*del)(lst->content);
 	free(lst);
