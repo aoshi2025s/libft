28cd7aa14ceb60adede952f11ce1b812410aea92
Yoshihiko Aoki <yoaoki@c1r6s1.42tokyo.jp>
2024-04-24 20:43:07 +0900

    strncmp donot segv

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: yoaoki <yoaoki@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 20:04:21 by yoaoki            #+#    #+#             */
-/*   Updated: 2024/04/24 17:32:13 by yoaoki           ###   ########.fr       */
+/*   Updated: 2024/04/24 20:42:43 by yoaoki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
+	if (!s1 || !s2)
+		return (0);
 	while ((*s1 || *s2) && n > 0)
 	{
 		if (*s1 != *s2)
