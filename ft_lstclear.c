ddcf6de872b7ef6f40a262a9144826557c14c683
aoshi2025s <aoshi2025s@gmail.com>
2024-04-24 14:05:09 +0900

    lstclear maybe ok

---
+++ ft_lstclear.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yoaoki <yoaoki@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/24 01:08:58 by yoaoki            #+#    #+#             */
+/*   Updated: 2024/04/24 14:03:56 by yoaoki           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*next_node;
+
+	if (!lst || !del)
+		return ;
+	while (*lst)
+	{
+		next_node = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		*lst = next_node;
+	}
+	free(*lst);
+	*lst = NULL;
+}
+/*
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	if (!del || !lst)
+		return ;
+	(*del)(lst->content);
+	free(lst);
+}
+*/
