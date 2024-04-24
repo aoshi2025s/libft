e5b5e33a468c52c105b12ada8b3acdd093b9446b
Yoshihiko Aoki <yoaoki@c1r6s1.42tokyo.jp>
2024-04-24 17:32:25 +0900

    strncmp modify

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: yoaoki <yoaoki@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 20:04:21 by yoaoki            #+#    #+#             */
-/*   Updated: 2024/04/18 17:38:12 by yoaoki           ###   ########.fr       */
+/*   Updated: 2024/04/24 17:32:13 by yoaoki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	while ((*s1 || *s2) && n)
+	while ((*s1 || *s2) && n > 0)
 	{
 		if (*s1 != *s2)
 			return (*(unsigned char *)s1 - *(unsigned char *)s2);
@@ -24,17 +24,3 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	}
 	return (0);
 }
-/*
-int	ft_strncmp(const char *s1, const char *s2, size_t n)
-{
-	while (*s1 == *s2 && n)
-	{
-		if (*s1 == '\0' && *s2 == '\0')
-			return (0);
-		s1++;
-		s2++;
-		n--;
-	}
-	return (*(unsigned char *)s1 - *(unsigned char *)s2);
-}
-*/
