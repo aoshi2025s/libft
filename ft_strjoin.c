09ce55ee6d211a2d0f2f471011ef3ba30033c472
aoshi2025s <136766971+aoshi2025s@users.noreply.github.com>
2024-04-26 00:59:44 +0900

    ft_strjoin.c のmalloc失敗じのNULLを0表記に変更

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -22,7 +22,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	total_len = ft_strlen(s1) + ft_strlen(s2) + 1;
 	result = (char *)malloc(total_len);
 	if (!result)
-		return (NULL);
+		return (0);
 	ft_strlcpy(result, s1, total_len);
 	ft_strlcat(result, s2, total_len);
 	return (result);
